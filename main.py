e58025111aaa942782bdd773788962a057c9283c
matheusvirissimo <matheus.virissimo@unesp.br>
2025-06-28 20:23:09 -0300

    feat: main

---
+++ main.py
@@ -0,0 +1,5 @@
+from calculadora_ipv6 import apresentar_calculadora
+
+# Quando executarmos o arquivo main.py, o código dentro deste 'if' será iniciado.
+if __name__ == "__main__":
+    apresentar_calculadora()
